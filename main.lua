0df51405d2e80a0e97eaccc5df74cc8f800984e1
Timothy Ortega <timortega95@gmail.com>
2024-01-16 13:39:43 -0700

    fixed issue #3
    added an extra para to the OnEvent function that restricts the CLEU subevent to only "SPELL_CAST_SUCCESS" to prevent ticking from emoting

--- main.lua
+++ main.lua
@@ -8,15 +8,9 @@ function DriveByBuffs:OnEnable()
 	self:Print("DriveByBuffs RP Started")
 end
 
-local f = CreateFrame("Frame");
-f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-
-local emotes = {"AMAZE", "APPLAUD", "BASHFUL", "BLUSH", "BOW", "BURP", "CHEER", "CLAP", "FART", "HAIL", "HAPPY", "HUG", "KISS", "SALUTE", "SEXY", "THANK", "CUDDLE", "PRAISE", "COMMEND", "FLIRT"}
-function randEmote()
-	return emotes[math.random(1, #emotes)]
+function DriveByBuffs:OnDisable()
+	self:Print("DriveByBuffs RP Stopped")
 end
-
-local validBuffs = {"Arcane Intellect", "Power Word: Fortitude", "Mark of the Wild", "Thorns", "Blessing of Wisdom", "Blessing of Might", "Blessing of Kings"}
 
 local function locate(table, value)
 	for i = 1, #table do
@@ -26,17 +20,24 @@ local function locate(table, value)
 	end
 end
 
+local emotes = {"AMAZE", "APPLAUD", "BASHFUL", "BLUSH", "BOW", "BURP", "CHEER", "CLAP", "FART", "HAIL", "HAPPY", "HUG", "KISS", "SALUTE", "SEXY", "THANK", "CUDDLE", "PRAISE", "COMMEND", "FLIRT"}
+function randEmote()
+	return emotes[math.random(1, #emotes)]
+end
+
+local validBuffs = {"Arcane Intellect", "Power Word: Fortitude", "Mark of the Wild", "Thorns", "Blessing of Wisdom", "Blessing of Might", "Blessing of Kings"}
+
 local playerGUID = UnitGUID("player")
 
+local f = CreateFrame("Frame");
+f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 f:SetScript("OnEvent", function(self, event)
 	local ts, subevent, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, _, buff = CombatLogGetCurrentEventInfo()
+	print(CombatLogGetCurrentEventInfo())
 	if locate(validBuffs, buff) then 
-		if destGUID == playerGUID and sourceGUID ~= playerGUID then
+		if destGUID == playerGUID and sourceGUID ~= playerGUID and subevent == "SPELL_CAST_SUCCESS" then
 			DoEmote(randEmote(), sourceName)
 		end
 	end
 end)
 
-function DriveByBuffs:OnDisable()
-	self:Print("DriveByBuffs RP Stopped")
-end
