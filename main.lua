2ba09ebec6ada1235c6a9ad3954adbf54e8cb61f
Timothy Ortega <timortega95@gmail.com>
2024-02-03 10:37:37 -0700

    swapping around addon dependencies, gettin somewhere

--- main.lua
+++ main.lua
@@ -7,18 +7,18 @@ add options table; https://wowpedia.fandom.com/wiki/Using_the_Interface_Options_
 ]]
 
 DriveByBuffs = LibStub("AceAddon-3.0"):NewAddon("DriveByBuffs", "AceConsole-3.0", "AceEvent-3.0")
+local AC = LibStub("AceConfig-3.0")
+local ACD = LibStub("AceConfigDialog-3.0")
 
 local dbb_Version = "1.0.3"
 
 local dbb_Defaults = {
-	["Options"] = {
-		["MasterOnOff"] = "On",
-		["version"] = dbb_Version,
-		["debug"] = false,
+	profile = {
+		version = dbb_Version,
 	},
-};
+}
 
-function dbb_Loaded()
+--[[ function dbb_Loaded()
 	if (not dbb_Options) then
 		dbb_Options = dbb_Defaults["Options"];
 		DEFAULT_CHAT_FRAME:AddMessage("DriveByBuffs Options database not found. Generating...");
@@ -33,7 +33,7 @@ function dbb_Loaded()
 		dbb_Options_temp["version"] = dbb_Version;
 		dbb_Options = dbb_Options_temp;
 	end
-end
+end ]]
 
 local function locate(table, value)
 	for i = 1, #table do
@@ -44,20 +44,24 @@ local function locate(table, value)
 end
 
 local dbb_Options = {
-name = "DriveByBuffs",
-handler = DriveByBuffs,
-type = "group",
-args = {},
+	name = "DriveByBuffs",
+	handler = DriveByBuffs,
+	type = "group",
+	args = {},
 }
 
 function DriveByBuffs:OnInitialize()
-	self:Print("Thanks for using DriveByBuffs!")
-	DriveByBuffsDB = DriveByBuffsDB or CopyTable(dbb_Defaults)
-	self.db = DriveByBuffsDB
-	LibStub("AceConfig-3.0"):RegisterOptionsTable("DriveByBuffs", dbb_Options)
-	self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("DriveByBuffs", "DriveByBuffs")
+	self.db = LibStub("AceDB-3.0"):New("DriveByBuffsDB", defaults, true)
+	AC:RegisterOptionsTable("dbb_Options", dbb_Options)
+	self.optionsFrame = ACD:AddToBlizOptions("dbb_Options", "DriveByBuffs")
+
+	local profiles = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db)
+	AC:RegisterOptionsTable("DriveByBuffs_Profiles", profiles)
+	ACD:AddToBlizOptions("DriveByBuffs_Profiles", "Profiles", "DriveByBuffs")
+
 	self:RegisterChatCommand("dbb", "SlashCommand")
-	allEmoteOptions()
+
+	self:Print("Thanks for using DriveByBuffs!")
 end
 
 function DriveByBuffs:OnEnable()
@@ -72,19 +76,22 @@ function DriveByBuffs:SlashCommand(msg)
 	InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
 end
 
-function DriveByBuffs:isActive(info)
+function DriveByBuffs:getActive(info)
 	print("getter called")
-	return DriveByBuffs.db.isActive
+	return self.db.profile.value
 end
 
 function DriveByBuffs:setActive(info, value)
 	print("setter called")
-	DriveByBuffs.db.setActive = value
+	self.db.profile.active = value
 end
 
 local emotes = {"AMAZE", "APPLAUD", "BASHFUL", "BLUSH", "BOW", "BURP", "CHEER", "CLAP", "FART", "HAIL", "HAPPY", "HUG", "KISS", "SALUTE", "SEXY", "THANK", "CUDDLE", "PRAISE", "COMMEND", "FLIRT"}
+
+local custEmotes = emotes
+
 function randEmote()
-	return emotes[math.random(1, #emotes)]
+	return emotes[math.random(1, #custEmotes)]
 end
 
 function allEmoteOptions()
@@ -93,8 +100,8 @@ function allEmoteOptions()
 			type = "toggle",
 			name = "",
 			desc = "Toggles the emote bank to include ",
-			get = DriveByBuffs.isActive(e),
-			set = DriveByBuffs.setActive(e, not value)
+			get = "getActive",
+			set = "setActive",
 		}
 		-- insert emote into a basic template
 		basics['name'] = string.lower(e)
