82bcb8e89a5568f8d75a6adfac5111580453b347
Timothy Ortega <timortega95@gmail.com>
2024-02-27 09:10:37 -0700

    changed how emotes work
    commented out native emote commands and DoEmote() to replace with filled strings to use the /me emote instead. This allows for emotes to be done actually on the run, instead of failing on the emotes that require an animation and resulting in a "cannot do that while moving" error.

--- main.lua
+++ main.lua
@@ -20,12 +20,20 @@ local function locate(table, value)
 	end
 end
 
-local emotes = {"AMAZE", "APPLAUD", "BASHFUL", "BLUSH", "BOW", "BURP", "CHEER", "CLAP", "FART", "HAIL", "HAPPY", "HUG", "KISS", "SALUTE", "SEXY", "THANK", "CUDDLE", "PRAISE", "COMMEND", "FLIRT"}
+--local emotes = {"AMAZE", "BLUSH", "BOW", "BURP", "CHEER", "CLAP", "FART", "HAIL", "HAPPY", "HUG", "KISS", "SALUTE", "SEXY", "THANK", "CUDDLE", "PRAISE", "COMMEND", "FLIRT"}
+local emotes = {"is amazed by ", "blushes at ", "bow before ", "cheers for ", "claps for ", "is happy with ", "hugs ", "blows a kiss at ", "salutes ", "thanks ", 
+								"cuddles up to ", "highly praises ", "gives many commendations to ", "flirts with "}
 function randEmote()
 	return emotes[math.random(1, #emotes)]
 end
 
-local validBuffs = {"Arcane Intellect", "Power Word: Fortitude", "Mark of the Wild", "Thorns", "Blessing of Wisdom", "Blessing of Might", "Blessing of Kings"}
+local validBuffs = {"Arcane Intellect", "Power Word: Fortitude", "Mark of the Wild", "Thorns", "Blessing of Wisdom", "Blessing of Might", "Blessing of Kings", "Blessing of Sanctuary", "Divine Spirit", "Shadow Protection"}
+--"Arcane Brilliance", "Gift of the Wild", "Greater Blessing of Might", "Greater Blessing of Widsom", "Greater Blessing of Kings", "Greater Blessing of Sanctuary", "Prayer of Fortitude", "Prayer of Spirit", "Focus Magic", "Power Infusion", "Tricks of the Trade", "Unholy Frenzy", "Divine Guardian"}
+
+local function createString(name, buff)
+	e = randEmote()
+	return e .. name .. " for their " .. buff .. "."
+end
 
 local playerGUID = UnitGUID("player")
 
@@ -37,7 +45,7 @@ f:SetScript("OnEvent", function(self, event)
 	
 	if locate(validBuffs, buff) then 
 		if destGUID == playerGUID and sourceGUID ~= playerGUID and subevent == "SPELL_CAST_SUCCESS" then
-			DoEmote(randEmote(), sourceName)
+			SendChatMessage(createString(sourceName, buff), "EMOTE")
 		end
 	end
 end)
